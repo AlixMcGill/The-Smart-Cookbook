df69e9f3d1c564840a2e8a1d0f413a08b35bf31e
Alix <Alix@LAPTOP-95BQ3G03>
2024-06-10 16:26:01 -0500

    Profile Page Switching

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -1,1 +1,48 @@
+// user tab selection
+const btnAbout = document.getElementById('button-about')
+const btnBio = document.getElementById('button-bio')
+const btnPantry = document.getElementById('button-pantry')
+const btnPosts = document.getElementById('button-posts')
+const btnFavorites = document.getElementById('button-favorites')
+
+const userBtnArr = [btnAbout, btnBio, btnPantry, btnPosts, btnFavorites]
+
+const userInfo = document.getElementById('user-info')
+const userBio = document.getElementById('user-bio')
+const userPantry = document.getElementById('user-pantry')
+const userPosts = document.getElementById('user-posts')
+const userFavorites = document.getElementById('user-favorites')
+
+const userContentArr = [userInfo, userBio, userPantry, userPosts, userFavorites]
+
+userBtnArr.forEach(btn => {
+    btn.addEventListener('click', () => {
+        //hide all content
+        userContentArr.forEach(contentItem => {
+            contentItem.hidden = true
+        })
+
+        switch (btn) {
+            case btnAbout:
+                userInfo.hidden = false
+                break
+            case btnBio:
+                userBio.hidden = false
+                break
+            case btnPantry:
+                userPantry.hidden = false
+                break
+            case btnPosts:
+                userPosts.hidden = false
+                break
+            case btnFavorites:
+                userFavorites.hidden = false
+                break
+        }
+    })
+})
+
+
+
 // Profile Pantry 
+
